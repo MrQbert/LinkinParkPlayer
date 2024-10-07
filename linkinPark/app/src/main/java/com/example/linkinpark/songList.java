30cb7d0e458f71d63172c6d23c133f238942b187
MrQbert <65645542+MrQbert@users.noreply.github.com>
2024-10-07 13:02:34 -0500

    more progress
    Working Media Player with buttons that toggle between play and pause

--- linkinPark/app/src/main/java/com/example/linkinpark/songList.java
+++ linkinPark/app/src/main/java/com/example/linkinpark/songList.java
@@ -2,13 +2,60 @@ package com.example.linkinpark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class songList extends AppCompatActivity {
+    ImageButton playNumbEl;
+    ImageButton playPapercutEl;
+    MediaPlayer numb;
+    MediaPlayer papercut;
+    Boolean isPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
+
+        numb = MediaPlayer.create(this,R.raw.numb);
+        papercut = MediaPlayer.create(this,R.raw.numb);
+        playNumbEl = findViewById(R.id.playNumbEl);
+        playPapercutEl = findViewById(R.id.playPapercutEl);
+        isPlaying = false;
     }
+    public void play_pause(View v){
+        if(v.getId() == R.id.playNumbEl){
+            if(!isPlaying){
+                numb.start();
+                playNumbEl.setImageResource(R.drawable.pause);
+                playPapercutEl.setEnabled(false);
+                isPlaying= true;
+            }else{
+                numb.pause();
+                playNumbEl.setImageResource(R.drawable.play);
+                playPapercutEl.setEnabled(true);
+                isPlaying= false;
+            }
+        }
+        else if(v.getId()== R.id.playPapercutEl){
+            if(!isPlaying) {
+                papercut.start();
+                playPapercutEl.setImageResource(R.drawable.pause);
+                playNumbEl.setEnabled(false);
+                isPlaying = true;
+            }else{
+                papercut.pause();
+                playPapercutEl.setImageResource(R.drawable.play);
+                playNumbEl.setEnabled(true);
+                isPlaying= false;
+
+            }
+
+        }
+
+
+    }
+
 }
